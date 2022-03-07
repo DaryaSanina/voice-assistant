10e5a58d5559f13686fcac2f4ec859737374aa7e
darya_sanina <dasha.sanina.07@gmail.com>
2022-03-07 14:15:44 +0300

    Added displaying messages

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+from messages import Message
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    sent_messages = list()
+    for i in range(100):
+        if i % 3 == 0:
+            sent_messages.append(Message(f'message {i}', 'user'))
+        else:
+            sent_messages.append(Message(f'message {i}', 'assistant'))
+    return render_template('main_page.html', messages=sent_messages)
 
 
 if __name__ == '__main__':
