5f3b5d4fc26ec64ec0a5490fb2f6d0eeac7f3cf6
darya_sanina <dasha.sanina.07@gmail.com>
2022-03-15 22:01:34 +0300

    Added ability to send messages (the assistant responses by repeating your message)

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
-from messages import Message
+from flask import Flask, render_template, redirect
+from messages import Message, MessageInputForm
+from assistant import Assistant
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(hash('secret_key'))
+assistant = Assistant()
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def main():
-    sent_messages = list()
-    for i in range(100):
-        if i % 3 == 0:
-            sent_messages.append(Message(f'message {i}', 'user'))
-        else:
-            sent_messages.append(Message(f'message {i}', 'assistant'))
-    return render_template('main_page.html', messages=sent_messages)
+    message_input_form = MessageInputForm()
+    if message_input_form.validate_on_submit():
+        sent_messages.append(Message(message_input_form.text.data, 'user'))
+        sent_messages.append(Message(assistant.answer(message_input_form.text.data), 'assistant'))
+        return redirect('/')
+    return render_template('main_page.html', messages=sent_messages, form=message_input_form)
 
 
 if __name__ == '__main__':
+    sent_messages = list()
     app.run(port=8000, host='127.0.0.1')
--- messages.py
+++ messages.py
@@ -1,4 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
 class Message:
     def __init__(self, text, sender):
         self.text = text
         self.sender = sender
+
+    def __repr__(self):
+        return self.text
+
+
+class MessageInputForm(FlaskForm):
+    text = StringField(validators=[DataRequired()], render_kw={"placeholder": "Type your message"})
