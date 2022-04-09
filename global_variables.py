efaf6587f0213a27f9e3a2bb7cb799e5207a5294
darya_sanina <dasha.sanina.07@gmail.com>
2022-04-09 13:55:26 +0300

    Update host and port

--- global_variables.py
+++ global_variables.py
@@ -1,6 +1,7 @@
 import spacy
+import os
 
-SERVER_ADDRESS_HOST = '127.0.0.1'
-SERVER_ADDRESS_PORT = 8000
+SERVER_ADDRESS_HOST = '0.0.0.0'
+SERVER_ADDRESS_PORT = int(os.environ.get("PORT", 5000))
 
 nlp = spacy.load('en_core_web_lg')
