471f27a4054378f275caa04e4776c9363318390c
darya_sanina <dasha.sanina.07@gmail.com>
2022-04-23 18:59:34 +0300

    -

--- global_variables.py
+++ global_variables.py
@@ -4,7 +4,7 @@ import os
 SERVER_ADDRESS_HOST = '0.0.0.0'
 SERVER_ADDRESS_PORT = int(os.environ.get("PORT", 5000))
 
-try:
+try: 
     nlp = spacy.load("en_core_web_md")
 except:
     spacy.cli.download("en_core_web_md")
